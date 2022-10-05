2244a8b6cef32a8871beeda7dfaf3cc4d07ced19
Solomon Sefa (sask) <kwabena81@yahoo.com>
2022-10-05 08:08:05 -0400

    second ...

---
+++ customer/src/main/java/com/asksef/customer/CustomerApplication.java
@@ -0,0 +1,11 @@
+package com.asksef.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String... strings) {
+        SpringApplication.run(CustomerApplication.class, strings);
+    }
+}
